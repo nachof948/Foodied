6bdd54f608f27bd8f74dd3b193c855d296584e33
nachof948 <nachof948@gmail.com>
2023-12-13 15:21:40 -0300

    cambios

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"https://foodied-server-nacho-fernandezs-projects.vercel.app/auth/google/callback",
+    callbackURL:"https://foodied-server.vercel.app/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) =>{
         try{
