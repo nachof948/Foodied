483ca7431f08f942fa65e016b78c547849b51739
nachof948 <nachof948@gmail.com>
2023-12-14 09:28:15 -0300

    jsajaja

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,49 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Route, Routes} from 'react-router-dom'
 import { TodasLasComidas, Carnes, Ensaladas, Sushi, Pastas, Pizzas, Sopas, Dulces, Veganos, Hamburguesas, Home, Producto, Registrarme, Carrito, CompraRealizada} from './indice';
-
+import axios from 'axios';
 function App() {
-
+  const [userGoogle, setUserGoogle] = useState(null)
+  
+  useEffect(() => {
+    const obtenerUsuario = async () => {
+      try {
+        const response = await axios.get('https://foodied-server.vercel.app/auth/exito');
+        console.log('El usuario es:', response.data)
+        if (response.status === 200) {
+          if (response.data && response.data.user) {
+            setUserGoogle(response.data.user); // Usuario autenticado
+          } else {
+            setUserGoogle(null); // Usuario no autenticado
+          }
+        } else {
+          throw new Error('Error en la autentificación');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    obtenerUsuario();
+  }, []);
   return (
   <div className="App">
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/comidas/all' element={<TodasLasComidas/>}></Route>
-        <Route path='/comidas/carnes' element={<Carnes />}></Route>
-        <Route path='/comidas/ensaladas' element={<Ensaladas />}></Route>
-        <Route path='/comidas/sushi' element={<Sushi />}></Route>
-        <Route path='/comidas/pastas' element={<Pastas  />}></Route>
-        <Route path='/comidas/pizzas' element={<Pizzas />}></Route>
-        <Route path='/comidas/veganos' element={<Veganos />}></Route>
-        <Route path='/comidas/sopas' element={<Sopas />}></Route>
-        <Route path='/comidas/dulces' element={<Dulces />}></Route>
-        <Route path='/comidas/hamburguesas' element={<Hamburguesas />}></Route>
-        <Route path='/producto/:_id' element={<Producto />}></Route>
+        <Route path='/' element={<Home userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/all' element={<TodasLasComidas userGoogle={userGoogle}/>}></Route>
+        <Route path='/comidas/carnes' element={<Carnes userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/ensaladas' element={<Ensaladas userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/sushi' element={<Sushi userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/pastas' element={<Pastas userGoogle={userGoogle}  />}></Route>
+        <Route path='/comidas/pizzas' element={<Pizzas userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/veganos' element={<Veganos userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/sopas' element={<Sopas userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/dulces' element={<Dulces userGoogle={userGoogle} />}></Route>
+        <Route path='/comidas/hamburguesas' element={<Hamburguesas userGoogle={userGoogle} />}></Route>
+        <Route path='/producto/:_id' element={<Producto userGoogle={userGoogle} />}></Route>
         <Route path='/auth/registrarse' element={<Registrarme />}></Route>
-        <Route path='/compras' element={<Carrito />}></Route>
+        <Route path='/compras' element={<Carrito userGoogle={userGoogle} />}></Route>
         <Route path='/compra-realizada' element={<CompraRealizada />}></Route>
         
       </Routes>
