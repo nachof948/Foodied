246f7f8b202603667e4b4e23e1d558cf7c9dc21c
nachof948 <nachof948@gmail.com>
2023-12-14 09:19:39 -0300

    Prubeasssss

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Elegirnos, Footer, Header, Hero, MenuEspecial, MenuHabitual, Opciones, SobreNosotros } from '../../indice';
+import axios from 'axios'
 import './Hero/Hojas de estilo/Hero.css'
-const Home = ({ userGoogle }) => {
+const Home = () => {
   const [loading, setLoading] = useState(true);
+  const [userGoogle, setUserGoogle] = useState(null)
 
   useEffect(() => {
     // Simulando una carga de datos con un temporizador
@@ -13,6 +15,28 @@ const Home = ({ userGoogle }) => {
     return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta antes de que termine la carga
   }, []);
 
+  
+  useEffect(() => {
+    const obtenerUsuario = async () => {
+      try {
+        const response = await axios.get('https://foodied-server.vercel.app/auth/exito');
+        console.log('El usuario es:', response.data)
+        if (response.status === 200) {
+          if (response.data && response.data.user) {
+            setUserGoogle(response.data.user); // Usuario autenticado
+          } else {
+            setUserGoogle(null); // Usuario no autenticado
+          }
+        } else {
+          throw new Error('Error en la autentificación');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    obtenerUsuario();
+  }, []);
   return (
     <>
       {loading ? (
