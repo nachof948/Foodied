61706151c4a5d93ccce1c38149bc5fe6812bf016
nachof948 <nachof948@gmail.com>
2023-12-14 08:46:30 -0300

    Pruebas cors

--- server/routes/registrarse.js
+++ server/routes/registrarse.js
@@ -7,15 +7,19 @@ router.get('/error', (req, res) => {
     res.status(401).json({message:"Error al registarse"})
 })
 router.get('/exito', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*')
+    const allowedOrigins = ['https://foodied-restaurante.vercel.app/', 'https://foodied-restaurante.vercel.app/auth/registrarse'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
     if(req.user){
         res.status(200).json({
             success: true,
-            message:"Exito al registrase",
+            message:"Exito al registrarse",
             user:req.user,
-        })
+        });
     }
-})
+});
 
 router.get('/logout', (req, res) => {
     req.logout()
