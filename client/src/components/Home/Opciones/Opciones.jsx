869d25c039516df012377dad15c9a139ea0d7020
nachof948 <nachof948@gmail.com>
2023-12-18 11:18:31 -0300

    Loading Opciones

--- client/src/components/Home/Opciones/Opciones.jsx
+++ client/src/components/Home/Opciones/Opciones.jsx
@@ -6,12 +6,17 @@ import { agregarAlCarrito } from '../../../Funciones/agregarProducto';
 import './Hojas de estilo/Opciones.css'
 
 const Opciones = ({usuarioLogueado}) => {
+  const [loading, setLoading] = useState(true)
   const [opciones, setOpciones] = useState([])
   const navegar = useNavigate()
   useEffect(() => {
     axios.get('https://foodied-server.vercel.app/home')
     .then((response) =>{
+      const delay = setTimeout(() => {
+        setLoading(false); // Actualiza el estado de carga después del tiempo de espera
+      }, 1000);
       setOpciones(response.data.comidas)
+      return () => clearTimeout(delay);
     })
     .catch((error) =>{console.error(error)})
     
@@ -21,29 +26,34 @@ const Opciones = ({usuarioLogueado}) => {
   return(
     <section id='opciones' className='seccion-opciones' >
       <div className='contenedor-opciones'>
-        {opciones.slice(0, 4).map(opcion =>{
-          const {nombre, descripcion, precio,imgUrl, _id} = opcion
-          return(
-            <div key={_id}>
-            <div className='tarjeta tarjeta-producto' onClick={()=>{mirarProducto(_id, navegar)}} > 
-              <img src={imgUrl} alt={nombre} loading="lazy" />
-              <div className="tarjeta-textos">
-                <h2>{nombre}</h2>
-                <p>{descripcion}</p>
-                
-              </div>
-            </div>
-            <div className="opciones-comprar">
-                  <p>${precio}</p>
-                  {usuarioLogueado ? (
-                      <button className='comprar-producto' onClick={()=>{agregarAlCarrito(_id, navegar)}}>Comprar Ahora</button>
-                  ) : (
-                    <a className='comprar-producto' href='/auth/registrarse'>Comprar Ahora</a>
-                  )}
+        {loading ? (
+          <div className="spinner" style={{margin:"0 auto"}}></div>
+        ):(
+          opciones.slice(0, 4).map(opcion =>{
+            const {nombre, descripcion, precio,imgUrl, _id} = opcion
+            return(
+              <div key={_id}>
+              <div className='tarjeta tarjeta-producto' onClick={()=>{mirarProducto(_id, navegar)}} > 
+                <img src={imgUrl} alt={nombre} loading="lazy" />
+                <div className="tarjeta-textos">
+                  <h2>{nombre}</h2>
+                  <p>{descripcion}</p>
+                  
                 </div>
-            </div> 
-          )
-        })}
+              </div>
+              <div className="opciones-comprar">
+                    <p>${precio}</p>
+                    {usuarioLogueado ? (
+                        <button className='comprar-producto' onClick={()=>{agregarAlCarrito(_id, navegar)}}>Comprar Ahora</button>
+                    ) : (
+                      <a className='comprar-producto' href='/auth/registrarse'>Comprar Ahora</a>
+                    )}
+                  </div>
+              </div> 
+            )
+          })
+        )}
+        
       </div>
     </section>
   )
