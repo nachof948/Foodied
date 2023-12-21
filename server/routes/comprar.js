6fcb0acaea7588d941e4f9f02bca0a59c192831c
nachof948 <nachof948@gmail.com>
2023-12-21 15:54:57 -0300

    route

--- server/routes/comprar.js
+++ server/routes/comprar.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 const { compra } = require('../controllers/comprar');
 
-router.post('/compra-realizada', compra)
+router.delete('/compra-realizada', compra)
 
 module.exports = router;
