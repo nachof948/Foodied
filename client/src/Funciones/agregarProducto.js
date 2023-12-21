5b07616d2b7e5aa692317627da66653555debb9f
nachof948 <nachof948@gmail.com>
2023-12-21 19:35:13 -0300

    otra vez token

--- client/src/Funciones/agregarProducto.js
+++ client/src/Funciones/agregarProducto.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-const agregarAlCarrito = (productoId,navegar) => {
+const agregarAlCarrito = (productoId,token,navegar) => {
   // Realizar una solicitud al servidor para agregar el producto al carrito por su ID
-    axios.post('https://foodied-server.vercel.app/compras/agregar', { productoId }/* ,{
+    axios.post('https://foodied-server.vercel.app/compras/agregar', { productoId },{
       headers:{
         Authorization:`Bearer ${token}`
       }
-    } */)
+    })
       .then(() => {
         navegar('/compras')
       })
