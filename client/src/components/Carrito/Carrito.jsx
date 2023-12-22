b185665c93e3ecb1a2771b02980e8a3ff378e8f7
nachof948 <nachof948@gmail.com>
2023-12-22 11:27:20 -0300

    sumar

--- client/src/components/Carrito/Carrito.jsx
+++ client/src/components/Carrito/Carrito.jsx
@@ -77,14 +77,13 @@ const Carrito = ({ usuarioLogueado, username, token }) => {
   
 
   const sumarProducto = async (productoId, token, carrito, setCarrito, setTotal) => {
-    console.log('Token:', token._id);
     try {
       await axios.post('https://foodied-server.vercel.app/compras/sumar', { id: productoId },{
         headers:{
           Authorization:`Bearer ${token}`
         }
       });
-  
+        console.log(token)
       const updatedCarrito = carrito.map((item) => {
         const updatedItems = item.items.map((producto) => {
           if (producto._id === productoId) {
