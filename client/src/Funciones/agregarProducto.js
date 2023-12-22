849645a56274d857887701c02d77d10bb938ca52
nachof948 <nachof948@gmail.com>
2023-12-22 11:47:58 -0300

    prueba

--- client/src/Funciones/agregarProducto.js
+++ client/src/Funciones/agregarProducto.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const agregarAlCarrito = (productoId,token,navegar) => {
+  console.log(token.id)
   // Realizar una solicitud al servidor para agregar el producto al carrito por su ID
     axios.post('https://foodied-server.vercel.app/compras/agregar', { productoId },{
       headers:{
